0026fe00eacb3420634089f46bba6970063f053a
Vasil <vortexblast550@gmail.com>
2023-12-12 20:01:00 +0200

    changes to composer.json

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,12 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+namespace Feature;
 use Tests\TestCase;
-
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
