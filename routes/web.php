0782b50959a8dcddb3e88f5ea5b93b45db614700
Vasil <vortexblast550@gmail.com>
2023-12-14 08:20:23 +0200

    change default view

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     Log::debug('hello-message',['test-context']);
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('kurec');
 });
+
