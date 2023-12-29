d113b00ab6b35b568753460fda52d25771c7f74a
themask025 <evelintodo@gmail.com>
2023-12-29 16:00:20 +0200

    Updated ec2-setup.sh with sudos

--- terraform/modules/webserver/ec2-setup.sh
+++ terraform/modules/webserver/ec2-setup.sh
@@ -15,17 +15,18 @@ sudo add-apt-repository ppa:ondrej/php &&
 sudo apt update && sudo apt upgrade -y &&
 sudo apt install php8.2-fpm &&
 sudo apt install php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-redis php8.2-intl -y &&
-sudo apt install composer nginx php8.2-mongo -y &&
+sudo apt install composer nginx php-mongo -y &&
 
 # Project setup
 cd /var/www/mdp_project &&
-cp .env.ci .env &&
+sudo cp .env.ci .env &&
+# composer update &&
 composer install &&
 php artisan key:generate &&
 php artisan migrate &&
 
 # Nginx setup
-cp /var/www/mdp_project/terraform/modules/webserver/nginx.conf /etc/nginx/sites-available/mdp-project &&
+sudo cp /var/www/mdp_project/terraform/modules/webserver/nginx.conf /etc/nginx/sites-available/mdp-project &&
 cd /etc/nginx/sites-enabled &&
 ln -s /etc/nginx/sites-available/mdp_project &&
 sudo systemctl start nginx
