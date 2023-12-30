69fb76eb14604389f9565cfeb6044b4fbf76d344
themask025 <evelintodo@gmail.com>
2023-12-30 13:24:44 +0200

    Updated ec2-setup.sh

--- terraform/modules/webserver/ec2-setup.sh
+++ terraform/modules/webserver/ec2-setup.sh
@@ -3,8 +3,12 @@
 [ `whoami` = root ] || exec su -c $0 root 
 
 # Cloning project
+sudo mkdir /var/www ||
 sudo mkdir /var/www/mdp_project && 
 sudo git clone https://github.com/VasilHristovDev/mdp_project /var/www/mdp_project && 
+
+# Disabling restarting of services after updates
+sudo apt remove needrestart -y
 
 # Installing dependencies
 sudo apt update && sudo apt upgrade -y &&
@@ -13,16 +17,16 @@ sudo add-apt-repository ppa:ondrej/php -y &&
 sudo apt update && sudo apt upgrade -y &&
 sudo apt install php8.2-fpm -y &&
 sudo apt install php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-redis php8.2-intl -y &&
-sudo apt install composer nginx php-mongo -y &&
+sudo apt install composer nginx php-mongodb -y &&
 
 # Project setup
 cd /var/www/mdp_project &&
 sudo cp .env.ci .env &&
-pecl install mongodb -y &&
-sudo echo "extension=mongodb.so" >> /etc/php/8.2/cli/php.ini &&
-sudo composer install -y &&
-php artisan key:generate &&
-php artisan migrate ||
+# printf "\n" | sudo pecl install mongodb &&
+# sudo echo "extension=mongodb.so" >> /etc/php/8.2/cli/php.ini &&
+printf "\n" | sudo composer install &&
+sudo php artisan key:generate &&
+sudo php artisan migrate ||
 
 # Nginx setup
 sudo cp /var/www/mdp_project/terraform/modules/webserver/nginx.conf /etc/nginx/sites-available/mdp_project &&
