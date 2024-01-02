d1f77e1752f9fa6616987b17154b46cb519ecdce
themask025 <evelintodo@gmail.com>
2024-01-02 13:32:27 +0200

    Updated ec2-setup.sh

--- terraform/modules/webserver/ec2-setup.sh
+++ terraform/modules/webserver/ec2-setup.sh
@@ -15,21 +15,20 @@ sudo apt update && sudo apt upgrade -y &&
 sudo apt install software-properties-common -y &&
 sudo add-apt-repository ppa:ondrej/php -y &&
 sudo apt update && sudo apt upgrade -y &&
-sudo apt install php8.2-fpm -y &&
-sudo apt install php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-redis php8.2-intl -y &&
-sudo apt install composer nginx php8.2-mongodb -y &&
+sudo apt install php8.2-fpm php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-redis php8.2-intl php8.2-mongodb composer nginx -y &&
 
 # Project setup
 cd /var/www/mdp_project &&
 sudo cp .env.ci .env &&
-# printf "\n" | sudo pecl install mongodb &&
-# sudo echo "extension=mongodb.so" >> /etc/php/8.2/cli/php.ini &&
 printf "\n" | sudo composer install &&
 sudo php artisan key:generate &&
 sudo php artisan migrate ||
+sudo chmod 777 -R /var/www/mdp_project/storage &&
 
 # Nginx setup
 sudo cp /var/www/mdp_project/terraform/modules/webserver/nginx.conf /etc/nginx/sites-available/mdp_project &&
 cd /etc/nginx/sites-enabled &&
 sudo ln -s /etc/nginx/sites-available/mdp_project &&
+sudo rm /etc/nginx/sites-available/default &&
+sudo rm /etc/nginx/sites-enabled/default &&
 sudo systemctl start nginx
