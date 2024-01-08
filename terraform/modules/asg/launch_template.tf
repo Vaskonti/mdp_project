31d10df5ba84f71134372a72806856261ac9177f
kaisiq <dahudohu@gmail.com>
2024-01-08 20:45:20 +0200

    Rename file to launch_template; add versioning and use latest; add iam role for webservers

--- terraform/modules/asg/launch_template.tf
+++ terraform/modules/asg/launch_template.tf
@@ -6,6 +6,12 @@ resource "aws_launch_template" "webserverconfig" {
   security_group_names = [var.elb_security_group_name]
   user_data            = file("${path.module}/ec2-setup.sh")
 
+  iam_instance_profile {
+    arn = "arn:aws:iam::262736261154:role/WebServerRole"
+  }
+
+  update_default_version = true
+
   lifecycle {
     create_before_destroy = true
   }
