7418b166ea1d2b4662bc7db912683ec62f9c2abf
kaisiq <dahudohu@gmail.com>
2024-01-09 01:06:06 +0200

    Try to convert ec2 script into base64

--- terraform/modules/asg/launch_template.tf
+++ terraform/modules/asg/launch_template.tf
@@ -1,10 +1,14 @@
+data "template_file" "user_data_template" {
+  template = file("${path.module}/ec2-setup.sh")
+}
+
 resource "aws_launch_template" "webserverconfig" {
   name_prefix          = "web-"
   image_id             = "ami-0c7217cdde317cfec"
   instance_type        = "t2.micro"
   key_name             = "ccKP"
   security_group_names = [var.elb_security_group_name]
-  user_data            = file("${path.module}/ec2-setup.sh")
+  user_data            = base64encode(data.template_file.user_data_template.rendered)
 
   iam_instance_profile {
     arn = "arn:aws:iam::262736261154:instance-profile/WebServerRole"
