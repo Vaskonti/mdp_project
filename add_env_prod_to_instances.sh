9787c61fc817ca4bcc3799656272692ccf5674cd
Vasil <vortexblast550@gmail.com>
2024-01-13 14:25:38 +0200

    [s3_env_prod

--- add_env_prod_to_instances.sh
+++ add_env_prod_to_instances.sh
@@ -4,7 +4,7 @@ privateIps=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=webserve
 for ip in $privateIps
 do
     echo "Connecting to $ip"
-    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$ip && aws s3 cp s3://environment-laravel/env/.env /var/www/mdp_project/.env
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$ip && sudo aws s3 cp s3://environment-laravel/env/.env /var/www/mdp_project/.env && cd /var/www/mdp_project && sudo composer install && sudo php artisan key:generate
 done
 
 echo "Done"
