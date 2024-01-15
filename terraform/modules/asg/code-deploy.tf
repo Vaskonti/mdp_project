0ca2c9d6405e906968c7a8f86da4b9450aa8bb18
kaisiq <dahudohu@gmail.com>
2024-01-15 13:16:13 +0200

    Change termination time to 5min && swap out the arn for deployment group service role

--- terraform/modules/asg/code-deploy.tf
+++ terraform/modules/asg/code-deploy.tf
@@ -1,30 +1,5 @@
 # <-------- CodeDeploy ----------->
 
-resource "aws_iam_role" "mdp_project" {
-  name = "hello-world-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codedeploy.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-  role       = aws_iam_role.mdp_project.name
-}
-
 resource "aws_codedeploy_app" "mdp_project" {
   name = "mdp_project"
 }
@@ -35,18 +10,20 @@ resource "aws_codedeploy_deployment_group" "mdp_project" {
   # deployment_group_name = "laravel-app-deployment-group"
   deployment_group_name  = "mdp_project-deployment-group"
   deployment_config_name = "CodeDeployDefault.OneAtATime"
-  # service_role_arn      = "arn:aws:iam::262736261154:role/CodeDeployRole"
-  service_role_arn = aws_iam_role.mdp_project.arn
+  service_role_arn       = "arn:aws:iam::262736261154:role/CodeDeployRole"
+  #   service_role_arn = aws_iam_role.mdp_project.arn
 
   deployment_style {
     deployment_option = "WITH_TRAFFIC_CONTROL"
     deployment_type   = "BLUE_GREEN"
   }
 
+  autoscaling_groups = [aws_autoscaling_group.webserver.name]
+
   blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 2
+      termination_wait_time_in_minutes = 5
     }
 
     deployment_ready_option {
