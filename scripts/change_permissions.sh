9b6aa5e414abab95601e4c72719d28f509ca5c95
kaisiq <dahudohu@gmail.com>
2024-01-15 21:56:50 +0200

    Fix real big bug

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Project setup
-cd /var/www/mdp_project ||
-sudo aws s3 cp s3://environment-laravel/env/.env . ||
-sudo wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem ||
-printf "\n" | sudo composer clear-cache ||
-printf "\n" | sudo composer update ||
-sudo rm -rf /var/www/mdp_project/vendor ||
-printf "\n" | sudo composer install ||
-sudo php artisan key:generate ||
-sudo php artisan migrate ||
-sudo chmod 777 -R /var/www/mdp_project/storage ||
+cd /var/www/mdp_project &&
+sudo aws s3 cp s3://environment-laravel/env/.env . &&
+printf "\n" | sudo composer install &&
+sudo php artisan key:generate &&
+sudo php artisan migrate ;
+sudo chmod 777 -R /var/www/mdp_project/storage &&
 
 # Nginx setup
-sudo cp /var/www/mdp_project/terraform/modules/asg/nginx.conf /etc/nginx/sites-available/mdp_project ||
-cd /etc/nginx/sites-enabled ||
-sudo ln -s /etc/nginx/sites-available/mdp_project ||
-sudo rm /etc/nginx/sites-available/default ||
-sudo rm /etc/nginx/sites-enabled/default ||
-sudo systemctl start nginx ||
+sudo cp /var/www/mdp_project/terraform/modules/asg/nginx.conf /etc/nginx/sites-available/mdp_project &&
+cd /etc/nginx/sites-enabled &&
+sudo ln -s /etc/nginx/sites-available/mdp_project ;
+sudo rm /etc/nginx/sites-available/default &&
+sudo rm /etc/nginx/sites-enabled/default &&
+sudo systemctl start nginx &&
 sudo systemctl restart nginx
