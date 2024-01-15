05e8b2c09be9dcd084bc01cb3baf770e22a24432
Vasil <vortexblast550@gmail.com>
2024-01-15 12:52:31 +0200

    [final touches]

--- add_env_prod_to_instances.sh
+++ add_env_prod_to_instances.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-privateIps=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=webserver" --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text)
+privateIps=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=webserver" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
 for ip in $privateIps
 do
     echo "Connecting to $ip"
-    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$ip &&
-    sudo aws s3 cp s3://environment-laravel/env/.env /var/www/mdp_project/.env &&
-    cd /var/www/mdp_project &&
-    sudo composer install &&
-    sudo php artisan key:generate
+    ssh ubuntu@$ip 'sudo aws s3 cp s3://environment-laravel/env/.env /var/www/mdp_project/.env; cd /var/www/mdp_project; sudo composer install; sudo php artisan key:generate; exit'
 done
 
 echo "Done"
